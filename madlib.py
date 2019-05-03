94ad4f52dc3938122ebe6b6275cdb249b12e626b
Cheryl Quan <cheryl.quan@hotmail.com>
2019-05-03 19:07:10 -0400

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -22,10 +22,10 @@ credits: adapted from PhD Comics
 import os
 
 #person 1
-teamName = ""
-synonymForNew = ""
-sciencyVerb = ""
-buzzword = ""
+teamName = "Team Unicorn"
+synonymForNew = "Novel"
+sciencyVerb = "Research"
+buzzword = "Synergy"
 
 #person 2
 number = ""
